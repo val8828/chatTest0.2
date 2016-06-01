237deb0d8c61d563571d1d4718e7d7dd7caee920
val8828 <val8828@yandex.ru>
2016-06-01 18:29:19 +0400

    Changed message list. Need to bind button textfield. Need to change Controoler to ioc data template

--- src/main/java/javaFXTests/ControllerTVImpl.java
+++ src/main/java/javaFXTests/ControllerTVImpl.java
@@ -8,11 +8,16 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextBuilder;
 import javafx.util.Callback;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ControllerTVImpl implements GUIController {
@@ -51,7 +56,6 @@ public class ControllerTVImpl implements GUIController {
         name.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
         name.setPrefWidth(50);
 
-
         TableColumn<User, Integer> id = new TableColumn<>("Id");
         id.setCellValueFactory(new PropertyValueFactory<User, Integer>("id"));
         id.setPrefWidth(5);
@@ -60,41 +64,35 @@ public class ControllerTVImpl implements GUIController {
         online.setCellValueFactory(new PropertyValueFactory<User, Boolean>("online"));
         online.setPrefWidth(60);
 
-        icon.setCellFactory(param -> {
-            TableCell<User, Image> cell = new TableCell<User, Image>() {
-                @Override
-                public void updateItem(Image item, boolean empty) {
-                    if (item != null) {
-                        ImageView imageview = new ImageView();
-                        imageview.setFitHeight(50);
-                        imageview.setFitWidth(50);
-                        imageview.setImage(item);
+        icon.setCellFactory(param -> new TableCell<User, Image>() {
+            @Override
+            public void updateItem(Image item, boolean empty) {
+                if (item != null) {
+                    ImageView imageview = new ImageView();
+                    imageview.setFitHeight(50);
+                    imageview.setFitWidth(50);
+                    imageview.setImage(item);
 
-                        setGraphic(imageview);
-                    }
+                    setGraphic(imageview);
                 }
-            };
-            return cell;
+            }
         });
 
-        online.setCellFactory(param -> {
-            TableCell<User, Boolean> cell = new TableCell<User, Boolean>() {
-                @Override
-                public void updateItem(Boolean item, boolean empty) {
-                    if (item != null) {
-                        ImageView imageview = new ImageView();
-                        imageview.setFitHeight(50);
-                        imageview.setFitWidth(50);
-                        if (item) {
-                            imageview.setImage(new Image("online.png"));
-                        } else {
-                            imageview.setImage(new Image("offline.png"));
-                        }
-                        setGraphic(imageview);
+        online.setCellFactory(param -> new TableCell<User, Boolean>() {
+            @Override
+            public void updateItem(Boolean item, boolean empty) {
+                if (item != null) {
+                    ImageView imageview = new ImageView();
+                    imageview.setFitHeight(50);
+                    imageview.setFitWidth(50);
+                    if (item) {
+                        imageview.setImage(new Image("online.png"));
+                    } else {
+                        imageview.setImage(new Image("offline.png"));
                     }
+                    setGraphic(imageview);
                 }
-            };
-            return cell;
+            }
         });
 
         usersTableView.getColumns().clear();
@@ -102,37 +100,68 @@ public class ControllerTVImpl implements GUIController {
         usersTableView.getColumns().addAll(icon, name, online);
 
         usersTableView.setItems(App.getUserListModel().getUserList());
-
     }
 
-    public void drawMessageList(){
+    public void drawMessageList() {
 
         messageListView.setCellFactory(new Callback<ListView<Message>, ListCell<Message>>() {
 
             @Override
             public ListCell<Message> call(ListView<Message> arg0) {
                 return new ListCell<Message>() {
-
                     @Override
                     protected void updateItem(Message item, boolean bln) {
                         super.updateItem(item, bln);
                         if (item != null) {
-                            VBox vBox = new VBox(new Text(item.getSender().getName()),
-                                    new Text(item.getDate().toString()),
-                                    new Text(item.getText()));
+                            Text label = new Text(item.getSender().getName() + "  ( " + getShortDateString(item.getDate()) + " )  ");
+                            label.setFont(Font.font ("Comic", 10));
+                            VBox vBox = new VBox((label), new Text(item.getText()));
                             vBox.setSpacing(10);
                             setGraphic(vBox);
-                            setStyle("-fx-control-inner-background: blue");
-                            System.out.println(getStyle());
                         }
                     }
-
                 };
             }
-
         });
 
         messageListView.setItems(App.getMessageHistory().getMessageList());
+    }
+
+    /**
+     * Returns date in short or long format if current date differ more than day
+     *
+     * @param date other date
+     * @return date in short or long format
+     */
+    public String getShortDateString(Date date) {
+        String result;
+
+        Date currentDate = new Date();
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(date);
+
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        cal.setTime(currentDate);
+
+        int currentDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        long anotherDate = 1;
+        Locale local = new Locale("ru", "RU");
+
+        if (currentDate.getTime() > (date.getTime() + 60 * 60 * 24 * 1000 * anotherDate) ||
+                currentDay != day) {
+
+            DateFormat df = DateFormat.getDateInstance(DateFormat.DEFAULT, local);
+
+            result = df.format(date);
+        } else {
+            DateFormat df = DateFormat.getTimeInstance(DateFormat.DEFAULT, local);
+            result = df.format(date);
+        }
+        return result;
     }
 
 
